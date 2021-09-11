2cd3c6400ca697110faa41995c7030e7752fb70b
andrejpenskij <penskiy_a@bk.ru>
2021-09-11 16:23:02 +0400

    начало111

---
+++ TP3/src/main/java/j.java
@@ -0,0 +1,4 @@
+public class j {public static void main(String[] args) {
+    System.out.println("Hello!");
+}
+}
--- untitled/src/main/java/w.java
+++
@@ -1,4 +0,0 @@
-public class w {public static void main(String[] args) {
-    System.out.println("Hello B!");
-}
-}
